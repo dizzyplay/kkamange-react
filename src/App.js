9c902bcc2b4196ccfb7de8df4313992c3f3c0b28
dizzyplay <vovoboss@gmail.com>
2019-01-24 02:26:43 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,61 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+
+function App(props) {
+  let comment = [
+    {
+      name:'james',
+      comment:'hello hi'
+    },
+    {
+      name:'vovo',
+      comment:'hi there hi'
+    },
+    {
+      name:'dasom',
+      comment:'hi hi im hungry hi'
+    },
+  ];
+  const title = "hi this is title"
+
+  return (
+    <div className="main">
+      <ImgDiv
+        title={title}
+        imgUrl="http://kkamange.site/media/image/2019/01/14/86C0F8F2-6D50-41D8-B9E8-E5F115F64B3D.jpg"
+        comment={comment}
+      />
+    </div>
+  )
+}
+
+function ImgDiv(props) {
+  return (
+    <div className="mainImageDiv">
+      <h2>{props.title}</h2>
+      <ShowPhoto imgUrl={props.imgUrl} alt={props.alt}/>
+      <Comment comment={props.comment}/>
+    </div>
+  )
+}
+
+function Comment(props) {
+  const comment = props.comment;
+  const commentList = comment.map(c => <li>{c.name} - {c.comment}</li>);
+  return (
+    <div>
+      <ul>
+        {commentList}
+      </ul>
+    </div>
+  )
+}
+
+function ShowPhoto(props) {
+  return (
+    <img width="300" src={props.imgUrl} alt={props.alt}/>
+  )
 }
 
 export default App;
