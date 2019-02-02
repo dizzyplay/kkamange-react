22d96a207db1a04d02c9889e6e4178d8cab92d17
dizzyplay <vovoboss@gmail.com>
2019-02-02 13:50:39 +0900

    Add routes

--- src/components/App/presenter.js
+++ src/components/App/presenter.js
@@ -1,15 +1,36 @@
 import React from 'react';
+import {Route, Switch} from 'react-router-dom';
 import Footer from 'components/Footer'
+import LoginForm from 'components/LoginForm'
 import styles from './styles.scss'
 
-class App extends React.Component{
-  render(){
-    return(
+class App extends React.Component {
+  render() {
+    const {isLoggedIn} = this.props;
+    return (
       <div className={styles.App}>
+        {isLoggedIn ? <PrivateRoutes/> : <PublicRoutes/>}
         <Footer/>
       </div>
     )
   }
+}
+
+const PrivateRoutes = () => {
+  return (
+    <Switch>
+      <Route exact path={'/'} render={() => "login status"}/>
+      <Route path={'/profile'} render={() => "profile status"}/>
+    </Switch>
+  )
+};
+
+const PublicRoutes = () => {
+  return (
+    <Switch>
+      <Route exact path={'/'} component={LoginForm}/>
+    </Switch>
+  )
 }
 
 export default App;
