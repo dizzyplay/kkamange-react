f6bbad384b26a842f8df7424b025e81ca548e121
dizzyplay <vovoboss@gmail.com>
2019-02-09 10:42:00 +0900

    cleanup

--- src/components/SignUpForm/index.js
+++ src/components/SignUpForm/index.js
@@ -4,8 +4,8 @@ import {actionCreators as userActions} from 'redux/modules/user'
 
 const mapDispatchToProps = (dispatch)=>{
   return{
-    userSignup:(username,email,password1, password2, nickname, gender)=>{
-      dispatch(userActions.userSignup(username,email,nickname, password1,password2,gender))
+    userSignup:(username,email,password1, password2, nickname)=>{
+      dispatch(userActions.userSignup(username,email,nickname, password1,password2))
     }
   }
 }
